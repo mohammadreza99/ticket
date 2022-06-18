11ea057451c90e8a5fb1a8cc5f423e0d9f857508
PC-22 <shahbaziweb@gmail.com>
2022-06-18 11:55:52 +0430

    ..

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,12 +1,12 @@
-import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from '@angular/core';
-import {MainPage} from './main.page';
-import {OperatorPage} from './operator/operator.page';
-import {FAQPage} from './faq/faq.page';
-import {TicketPage} from './ticket/ticket.page';
-import {BugPage} from '@modules/main/bug/bug.page';
-import {DefaultAnswerPage} from '@modules/main/default-answer/default-answer.page';
-import {FAQCategoryPage} from '@modules/main/faq-category/faq-category.page';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { MainPage } from './main.page';
+import { OperatorPage } from './operator/operator.page';
+import { FAQPage } from './faq/faq.page';
+import { TicketPage } from './ticket/ticket.page';
+import { BugPage } from '@modules/main/bug/bug.page';
+import { DefaultAnswerPage } from '@modules/main/default-answer/default-answer.page';
+import { FAQCategoryPage } from '@modules/main/faq-category/faq-category.page';
 
 const routes: Routes = [
   {
@@ -16,27 +16,37 @@ const routes: Routes = [
       {
         path: 'bug',
         component: BugPage,
+        data: { title: 'bug' }
       },
       {
         path: 'default-answer',
         component: DefaultAnswerPage,
+        data: { title: 'default-answer' }
       },
       {
         path: 'faq',
         component: FAQPage,
+        data: { title: 'faq' }
       },
       {
         path: 'faq-category',
         component: FAQCategoryPage,
+        data: { title: 'faq-category' }
       },
       {
         path: 'operator',
         component: OperatorPage,
+        data: { title: 'operator' }
       },
       {
         path: 'ticket',
         component: TicketPage,
+        data: { title: 'ticket' }
       },
+      {
+        path: '',
+        redirectTo: 'bug'
+      }
     ],
   },
 ];
